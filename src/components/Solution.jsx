94fdaafc0fd755a0831ed653823e2980c5b6d796
remant <paudelremant45@gmail.com>
2024-01-21 22:27:45 +0545

    Solution component created

---
+++ src/components/Solution.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import NavBar from '../common/NavBar'
+import Footer from '../common/Footer'
+
+const Solution = () => {
+  return (
+    <>
+      <NavBar/>
+      ghgdshjf
+      dgsfhd
+
+      <div className='fixed bottom-0 left-0 w-full'>
+      <Footer/>
+      </div>
+    </>
+  )
+}
+
+export default Solution
