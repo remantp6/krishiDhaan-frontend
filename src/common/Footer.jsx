ab373c1979387002982478ea5190eb3343b84ad6
remant <paudelremant45@gmail.com>
2024-01-21 22:28:56 +0545

    src/common/Footer.jsx

--- src/common/Footer.jsx
+++ src/common/Footer.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Footer = () => {
   return (
     <>
-      <p className='flex flex-grow justify-center items-center bg-slate-900 px-24 text-white h-16 '>@copy; 2024 KrishiDhaan</p>
+      <p className='flex flex-grow justify-center items-center bg-slate-900 px-24 text-white h-16'>@copy; 2024 KrishiDhaan</p>
     </>
   )
 }
